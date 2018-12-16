b5d27492a34166817d9569c3a0ce592e27a9032e
Guilherme <gui.mochila@gmail.com>
2018-12-16 15:37:47 +0100

    StoreController added.

---
+++ controllers/storeController.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+exports.homePage = (req, res) => {
+  res.render('index', { username: req.name });
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save();
+
+  req.flash(
+    'success',
+    `Successfully Created ${store.name}. Care to leave a review?`,
+  );
+  res.redirect(`/store/${store.slug}`);
+};
