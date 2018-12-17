ad205be416a3e87446f11f4c3d3832f8e99f11eb
Guilherme <gui.mochila@gmail.com>
2018-12-17 15:26:18 +0100

    Workflow to edit stores.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -23,3 +23,23 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
 };
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true,
+  }).exec();
+  req.flash(
+    'success',
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${
+      store.slug
+    }">View Store </a>`,
+  );
+  res.redirect(`/store/${store.id}`);
+};
