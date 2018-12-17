5668147104a6339a9ed0c358f2a4b6e9a7e0cec3
Guilherme <gui.mochila@gmail.com>
2018-12-17 16:02:45 +0100

    Coordinates & Location added.

--- models/Store.js
+++ models/Store.js
@@ -14,6 +14,21 @@ const storeSchema = new mongoose.Schema({
     trim: true,
   },
   tags: [String],
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point',
+    },
+    coordinates: [{ type: Number, required: 'You must supply coordinates' }],
+    address: {
+      type: String,
+      required: 'You must supply an address!',
+    },
+  },
 });
 
 storeSchema.pre('save', function(next) {
