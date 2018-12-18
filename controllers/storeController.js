da17c1d5ff3961d5106f1858d57644fe12bb1f29
Guilherme <gui.mochila@gmail.com>
2018-12-18 11:31:29 +0100

    Ensure the update set type = "Point".

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -31,6 +31,7 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  req.body.location.type = 'Point';
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true,
     runValidators: true,
