a926943218ac3960a62534b7ac33c13bd0c8e005
Guilherme <gui.mochila@gmail.com>
2018-12-19 10:56:57 +0100

    Added Tag view and Tags selection

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -84,3 +84,9 @@ exports.getStoreBySlug = async (req, res, next) => {
 
   res.render('store', { store, title: store.name });
 };
+
+exports.getStoresByTag = async (req, res, next) => {
+  const tags = await Store.getTagsList();
+  const tag = req.params.tag;
+  res.render('tag', { tags, title: 'Tags', tag });
+};
--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,9 @@ router.post(
   catchErrors(storeController.updateStore),
 );
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
-
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 module.exports = router;
