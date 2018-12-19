2326489731a4e1b30040d6f361060560b78682c8
Guilherme <gui.mochila@gmail.com>
2018-12-19 10:33:52 +0100

    Fixing store slug duplicates

--- models/Store.js
+++ models/Store.js
@@ -32,12 +32,19 @@ const storeSchema = new mongoose.Schema({
   photo: String,
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     next();
     return;
   }
   this.slug = slug(this.name);
+  const slugRegex = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegex });
+
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
+
   next();
 });
 
