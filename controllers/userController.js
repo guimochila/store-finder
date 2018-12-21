a6b65e2371c053838a3e80eeeb141f2eaa62972c
Guilherme <gui.mochila@gmail.com>
2018-12-21 14:55:06 +0100

    Registration form and routes added.

---
+++ controllers/userController.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+exports.loginForm = (req, res) => {
+  res.render('login', { title: 'Login' });
+};
+
+exports.registerForm = (req, res) => {
+  res.render('register', { title: 'Registration' });
+};
+
+exports.validateRegister = (req, res, next) => {
+  req.sanitizeBody('name');
+  req.checkBody('name', 'You must supply a name!').notEmpty();
+  req.checkBody('email', 'That Email is not valid').isEmail();
+  req.sanitizeBody('email').normalizeEmail({
+    remove_dots: false,
+    remove_extension: false,
+    gmail_remove_subaddress: false,
+  });
+  req.checkBody('password', 'Password Cannot be Blank!').notEmpty();
+  req.checkBody('confirm-password', 'Confirmed Password can not be blank!');
+  req
+    .checkBody('confirm-password', 'Oops! Your passwords do not match')
+    .equals(req.body.password);
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('error', errors.map(err => err.msg));
+    res.render('register', {
+      title: 'Register',
+      body: req.body,
+      flashes: req.flash(),
+    });
+    return;
+  }
+  next();
+};
