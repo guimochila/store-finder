be162ea8d6e65fad24f9817b6bc7acde236a4345
Guilherme <gui.mochila@gmail.com>
2018-12-27 21:22:01 +0100

    Locking down the app with User Permissions

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -42,6 +42,7 @@ exports.resize = async (req, res, next) => {
 };
 
 exports.createStore = async (req, res) => {
+  req.body.author = req.user._id;
   const store = await new Store(req.body).save();
 
   req.flash(
@@ -56,8 +57,16 @@ exports.getStores = async (req, res) => {
   res.render('stores', { title: 'Stores', stores });
 };
 
+const confirmOwner = (store, user) => {
+  if (!store.author.equals(user._id)) {
+    throw Error('You must own a store in order to edit it!');
+  }
+};
+
 exports.editStore = async (req, res) => {
   const store = await Store.findOne({ _id: req.params.id });
+
+  confirmOwner(store, req.user);
 
   res.render('editStore', { title: `Edit ${store.name}`, store });
 };
@@ -78,7 +87,9 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoreBySlug = async (req, res, next) => {
-  const store = await Store.findOne({ slug: req.params.slug });
+  const store = await Store.findOne({ slug: req.params.slug }).populate(
+    'author',
+  );
 
   if (!store) return next();
 
