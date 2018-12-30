19e2cb183618e7b71bc84ab715f8a680ddcc39c7
Guilherme <gui.mochila@gmail.com>
2018-12-30 15:51:10 +0100

    Pushing User data to our API - Hearting the Stores

--- models/User.js
+++ models/User.js
@@ -22,6 +22,7 @@ const userSchema = new Schema({
   },
   resetPasswordToken: String,
   resetPasswordExpires: Date,
+  hearts: [{ type: mongoose.Schema.ObjectId, ref: 'Store' }],
 });
 
 userSchema.virtual('gravatar').get(function() {
