199d0be51f8157e03ec3825c0809b128c2107da4
Guilherme <gui.mochila@gmail.com>
2019-07-30 15:45:04 +0200

    Move files to source directory

---
+++ src/start.js
@@ -0,0 +1,22 @@
+import  mongoose from 'mongoose';
+
+// import environmental variables from our variables.env file
+require('dotenv').config({ path: 'variables.env' });
+
+// Connect to our Database and handle any bad connections
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true});
+mongoose.connection.on('error', err => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
+// READY?! Let's go!
+require('./models/Store');
+require('./models/User');
+require('./models/Review');
+
+// Start our app!
+import app from './app';
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
